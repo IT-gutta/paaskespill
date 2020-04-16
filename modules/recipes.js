e7fe9360c7d75568a65eab16ad628e05bd7e7961
jorgensandhaug <55702375+jorgensandhaug@users.noreply.github.com>
2020-04-16 10:25:57 +0200

    recipes

--- modules/recipes.js
+++ modules/recipes.js
@@ -2,7 +2,7 @@ const variables = require("./variables")
 const Item = variables.Item
 let recipes = {}
 
-function addRecipe(recipes, grid, type, value, quantity){
+function addRecipe(grid, type, value, quantity){
     current = recipes
     for(i=0; i<grid.length; i++){
         if(current[grid[i]]==undefined){
@@ -30,8 +30,7 @@ function checkRecipe(currentRecipe){
 }
 
 
-addRecipe(recipes, [1,1,1,1,1,1,1,1,1], "block", 6, 7)
-addRecipe(recipes, [1,1,1,0,9,0,0,9,0], "pickaxe", 12, 1)
+
 
 function addRecipeSingleItem(val, type, outComeValue, quantity){
     for(let i = 0; i < 9; i++){
@@ -40,10 +39,18 @@ function addRecipeSingleItem(val, type, outComeValue, quantity){
             if(i==j) arr[j] = val
             else arr[j] = 0
         }
-        addRecipe(recipes, arr, type, outComeValue, quantity)
+        addRecipe(arr, type, outComeValue, quantity)
     }
 }
-addRecipeSingleItem()
+//pickaxe
+addRecipe([1,1,1,0,9,0,0,9,0], "pickaxe", 12, 1)
+
+//1 wood til 4 sticks
+addRecipeSingleItem(2, "material", 9, 4)
+
+//safe
+addRecipe([1,1,1,17,18,17,1,1,1], "interactable", 8, 1)
+
 
 
 module.exports = {checkRecipe}
